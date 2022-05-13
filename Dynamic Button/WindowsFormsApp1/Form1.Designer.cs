4a2e6a4ebc5a2528762bbf349766550a2094e993
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-05-13 09:26:21 +0300

    Dynamic Button

---
+++ Dynamic Button/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(198, 357);
+            this.LoadButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(157, 58);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "button1";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 455);
+            this.Controls.Add(this.LoadButton);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
+
---
+++ Dynamic Button/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+            //create 6 buttons
+            int x = 33, y = 29;
+            for (int i = 0; i < 6; i++)
+            {
+                if (i == 0)
+                {
+                    Button b = new Button();  //Create button
+                    b.Location = new Point(x, y);  // Set new location
+                    b.Name = "button_" + (i + 1).ToString();  // Set button name
+                    b.Text = "Click";
+                    b.Size = new Size(110, 42);
+                    b.Font = new Font("Minion Pro", 20); // Change Button Font
+                    b.Padding = new Padding(0);
+                    b.Click += new EventHandler(this.button_click);
+                    this.Controls.Add(b);
+                }
+                else
+                {
+                    if (i % 2 != 0)
+                    {
+                        x = x + 170; // create button on right side
+
+                        Button b = new Button();
+                        b.Location = new Point(x, y);
+                        b.Name = "button_" +(i + 1).ToString();
+                        b.Text = "Click";
+                        b.Size = new Size(110, 42);
+                        b.Font = new Font("Minion Pro", 20);
+                        b.Padding = new Padding(0);
+                        b.Click += new EventHandler(this.button_click);
+                        this.Controls.Add(b);
+                    }
+                    else
+                    {
+
+                        x = x - 170;/* create button on bottom of first */
+                        y = y + 83;
+
+                        Button b = new Button();
+                        b.Location = new Point(x, y);
+                        b.Name = "button_" +(i + 1).ToString();
+                        b.Text = "Click";
+                        b.Size = new Size(110, 42);
+                        b.Font = new Font("Minion Pro", 20);
+                        b.Padding = new Padding(0);
+                        b.Click += new EventHandler(this.button_click);
+                        this.Controls.Add(b);
+                    }
+                }
+            }
+        }
+        void button_click(object sender,EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Text = "clicked";            
+        }
+    }
+}
